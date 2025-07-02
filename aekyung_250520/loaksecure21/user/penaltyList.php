4298565c63943149f1130154011dd796ef0046b2
nari-jo <narijjo@musign.net>
2025-07-02 11:21:23 +0900

    회원관리>슈퍼패스 지급내역 / 패널티내역(퍼블리싱 작업본 업데이트)

--- aekyung_250520/loaksecure21/user/penaltyList.php
+++ aekyung_250520/loaksecure21/user/penaltyList.php
@@ -4,11 +4,11 @@ if(!defined('_HEROBOARD_'))exit;
 $search = "";
 
 if($_GET["type"]) {
-	$search .= " AND p.type = '".$_GET["type"]."' ";
+    $search .= " AND p.type = '".$_GET["type"]."' ";
 }
 
 if($_GET["kewyword"]) {
-	$search .= " AND ".$_GET["select"]." like '%".$_GET["kewyword"]."%' ";
+    $search .= " AND ".$_GET["select"]." like '%".$_GET["kewyword"]."%' ";
 }
 
 //페이지 넘버링
@@ -25,10 +25,10 @@ $list_page=20;
 $page_per_list=10;
 
 if(!strcmp($_GET['page'], '') || !strcmp($_GET['page'], '1')){
-	$page = '1';
+    $page = '1';
 }else{
-	$page = $_GET['page'];
-	$i = $i-($page-1)*$list_page;
+    $page = $_GET['page'];
+    $i = $i-($page-1)*$list_page;
 }
 
 $start = ($page-1)*$list_page;
@@ -45,11 +45,80 @@ $sql .= " ORDER BY p.hero_idx DESC LIMIT ".$start.",".$list_page;
 $list_res = sql($sql);
 
 $type_arr = array("1"=>"이중아이디","2"=>"가이드라인 미준수","3"=>"후기 미등록","4"=>"오프라인 모임 미참여","5"=>"풍평/설문 미진행","9"=>"기타");
-?>	
+?>
 <form name="searchForm" id="searchForm" action="<?=PATH_HOME.'?'.get('page');?>">
-<input type="hidden" name="idx" value="<?=$_GET["idx"]?>" />
-<input type="hidden" name="board" value="<?=$_GET["board"]?>" />
-<table class="tbSearch">
+    <input type="hidden" name="idx" value="<?=$_GET["idx"]?>" />
+    <input type="hidden" name="board" value="<?=$_GET["board"]?>" />
+
+    <!-- 250625 SNS 주소 확인 검색 필터 -->
+    <table class="searchBox">
+        <colgroup>
+            <col width="171px" />
+            <col width="*" />
+        </colgroup>
+        <tr>
+            <th>페널티 타입</th>
+            <td>
+                <div class="search_inner sup">
+                    <label class="akContainer">전체
+                        <input type="checkbox" name="penalty_type_chk" value="0">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">이중아이디
+                        <input type="checkbox" name="penalty_type_chk" value="1">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">가이드라인 미준수
+                        <input type="checkbox" name="penalty_type_chk" value="2">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">후기 미등록
+                        <input type="checkbox" name="penalty_type_chk" value="3">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">오프라인 모임 미참여
+                        <input type="checkbox" name="penalty_type_chk" value="4">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">품평/설문 미진행
+                        <input type="checkbox" name="penalty_type_chk" value="5">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">기타
+                        <input type="checkbox" name="penalty_type_chk" value="6">
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                검색
+            </th>
+            <td>
+                <div class="search_inner">
+                    <div class="select-wrap">
+                        <select name="select">
+                            <option value="none" <?=!isset($_GET["select"]) || $_GET["select"] == "none" ? "selected" : ""?>>선택</option>
+                            <option value="hero_memo" <?=$_GET["select"] == "m.memo" ? "selected" : ""?>>내용</option>
+                            <option value="hero_nick" <?=$_GET["select"] == "m.hero_nick" ? "selected" : ""?>>닉네임</option>
+                            <option value="hero_id" <?=$_GET["select"] == "m.hero_id" ? "selected" : ""?>>아이디</option>
+                            <option value="hero_name" <?=$_GET["select"] == "m.hero_name" ? "selected" : ""?>>이름</option>
+                        </select>
+                    </div>
+                    <input class="search_txt" type="text" name="kewyword" value="<?=$_GET["kewyword"]?>"/>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <div class="btnGroupSearch_box">
+        <div class="btnGroupSearch">
+            <a href="javascript:;" onClick="fnSearch()" class="btnSearch">검색</a>
+        </div>
+    </div>
+
+    <!-- 250625 검색필터 기존 주석처리 -->
+    <!-- <table class="tbSearch">
 	<colgroup>
 		<col width="150px" />
 		<col width="*" />
@@ -65,7 +134,7 @@ $type_arr = array("1"=>"이중아이디","2"=>"가이드라인 미준수","3"=>"후기 미등록",
 				<option value="4" <?=$_GET["type"]=="4" ? "selected":""?>>오프라인 모임 미참여</option>
 				<option value="5" <?=$_GET["type"]=="5" ? "selected":""?>>품평/설문 미진행</option>
 				<option value="9" <?=$_GET["type"]=="9" ? "selected":""?>>기타</option>
-			</select>	
+			</select>
 		</td>
 	</tr>
 	<tr>
@@ -77,105 +146,194 @@ $type_arr = array("1"=>"이중아이디","2"=>"가이드라인 미준수","3"=>"후기 미등록",
 		    	<option value="m.hero_name" <?if(!strcmp($_REQUEST['select'], 'm.hero_name')){echo ' selected';}else{echo '';}?>>이름</option>
 		    	<option value="m.hero_id" <?if(!strcmp($_REQUEST['select'], 'm.hero_id')){echo ' selected';}else{echo '';}?>>아이디</option>
 	    	</select>
-	    	<input name="kewyword" type="text" value="<?echo $_REQUEST['kewyword'];;?>" class="kewyword">		
+	    	<input name="kewyword" type="text" value="<?echo $_REQUEST['kewyword'];;?>" class="kewyword">
 		</td>
 	</tr>
 </table>
 <div class="btnGroupSearch">
 	<a href="javascript:;" onClick="fnSearch()" class="btnSearch">검색</a>
-</div>
+</div> -->
 </form>
 
-<div class="listExplainWrap mgb10">
-<label>총 </label> : <strong><?=number_format($total_data)?></strong>건
+<div class="tableSection mgt30">
+    <div class="table_top">
+        <h2 class="table_tit">검색 결과</h2>
+        <p class="postNum"><span class="line"><?=number_format($search_total)?>개</span><span class="op_5">전체 <?=number_format($total_data)?>개</span></p>
+    </div>
+    <p class="table_desc"></p>
+    <div class="searchResultBox_container">
+        <table class="searchResultBox">
+            <colgroup>
+                <col width="45px" />
+                <col width="70px" />
+                <col width="70px" />
+                <col width="70px" />
+                <col width="70px" />
+                <col width="70px" />
+                <col width="120px" />
+                <col width="75px" />
+            </colgroup>
+            <thead>
+            <th>
+                <div class="">
+                    NO
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    이름
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    아이디
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    닉네임
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    서포터즈 구분
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    페널티 타입
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    상세 내용
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    등록일
+                </div>
+            </th>
+            </thead>
+            <tbody>
+            <?
+            if($total_data > 0) {
+                while($list = mysql_fetch_assoc($list_res)) {
+                    $age = "";
+                    if($list["hero_jumin"]) {
+                        $age = (date("Y")-substr($list["hero_jumin"],0,4))+1;
+                    }
+
+                    $hero_sex_txt = "";
+                    if($list["hero_sex"] == 1) {
+                        $hero_sex_txt = "남";
+                    } else if(strlen($list["hero_sex"]) > 0 && $list["hero_sex"] == 0) {
+                        $hero_sex_txt = "여";
+                    }
+
+                    $hero_use_txt = "";
+                    if($list["hero_use"] == 0) {
+                        $hero_use_txt = "회원";
+                    } else if($list["hero_use"] == 1) {
+                        $hero_use_txt = "탈퇴";
+                    } else if($list["hero_use"] == 2) {
+                        $hero_use_txt = "휴먼회원";
+                    }
+                    ?>
+                    <tr>
+                        <td>
+                            <div class="table_result_no">
+                                <?=$i?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$list["hero_name"]?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$list["hero_id"]?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$list["hero_nick"]?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                서포터즈 구분
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_types">
+                                <?=$type_arr[$list["type"]]?>
+                            </div>
+                        </td>
+                        <td class="title">
+                            <div class="table_result_contents pop_btn_01">
+                                <?=$list["memo"]?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_create">
+                                <?=substr($list["hero_today"],0,10)?>
+                            </div>
+                        </td>
+                    </tr>
+                    <?
+                    $i--;
+                }
+            } else {
+                ?>
+                <tr>
+                    <td colspan="8" class="no_data">등록된 데이터가 없습니다.</td>
+                </tr>
+            <?}?>
+            </tbody>
+        </table>
+    </div>
 </div>
 
-<table class="t_list">
-<colgroup>
-	<col width="4%" />
-	<col width="7%" />
-	<col width="7%" />
-	<col width="7%" />
-	<col width="5%" />
-	<col width="5%" />
-	<col width="5%" />
-	<col width="5%" />
-	<col width="10%" />
-	<col width="*" />
-	<col width="15%" />
-</colgroup>
-<thead>
-<tr>
-	<th>NO</th>
-	<th>이름</th>
-	<th>아이디</th>
-	<th>닉네임</th>
-	<th>나이</th>
-	<th>성별</th>
-	<th>레벨</th>
-	<th>회원상태</th>
-	<th>패널티 타입</th>
-	<th>내용</th>
-	<th>등록일</th>
-</tr>
-</thead>
-<?
-if($total_data > 0) {
-while($list = mysql_fetch_assoc($list_res)) {
-	$age = "";
-	if($list["hero_jumin"]) {
-		$age = (date("Y")-substr($list["hero_jumin"],0,4))+1;
-	}
-	
-	$hero_sex_txt = "";
-	if($list["hero_sex"] == 1) {
-		$hero_sex_txt = "남";
-	} else if(strlen($list["hero_sex"]) > 0 && $list["hero_sex"] == 0) {
-		$hero_sex_txt = "여";
-	}
-	
-	$hero_use_txt = "";
-	if($list["hero_use"] == 0) {
-		$hero_use_txt = "회원";
-	} else if($list["hero_use"] == 1) {
-		$hero_use_txt = "탈퇴";
-	} else if($list["hero_use"] == 2) {
-		$hero_use_txt = "휴먼회원";
-	}
-?>
-<tr>
-	<td><?=$i?></td>
-	<td><?=$list["hero_name"]?></td>
-	<td><?=$list["hero_id"]?></td>
-	<td><?=$list["hero_nick"]?></td>
-	<td><?=$age?></td>
-	<td><?=$hero_sex_txt?></td>
-	<td><?=$list["hero_level"]?></td>
-	<td><?=$hero_use_txt?></td>
-	<td><?=$type_arr[$list["type"]]?></td>
-	<td class="title"><?=$list["memo"]?></td>
-	<td><?=substr($list["hero_today"],0,10)?></td>
-</tr>
-<? 
-$i--;
-} 
-} else {
-?>
-<tr>
-	<td colspan="11">등록된 데이터가 없습니다.</td>
-</tr>
-<?}?>
-</table>
+<!-- 페널티 상세 내용 팝업-->
+<div class="popup_url_box">
+    <div class="popup_url_cont height_typeB">
+        <div class="popup_url_head">
+            <svg class="close" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.41073 4.41083C4.73617 4.08539 5.26381 4.08539 5.58925 4.41083L15.5892 14.4108C15.9147 14.7363 15.9147 15.2639 15.5892 15.5893C15.2638 15.9148 14.7362 15.9148 14.4107 15.5893L4.41073 5.58934C4.0853 5.2639 4.0853 4.73626 4.41073 4.41083Z" fill="black"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M15.5892 4.41083C15.2638 4.08539 14.7362 4.08539 14.4107 4.41083L4.41072 14.4108C4.08529 14.7363 4.08529 15.2639 4.41072 15.5893C4.73616 15.9148 5.2638 15.9148 5.58924 15.5893L15.5892 5.58934C15.9147 5.2639 15.9147 4.73626 15.5892 4.41083Z" fill="black"/>
+            </svg>
+        </div>
+        <div class="popup_url_body">
+            <div class="tit">페널티 상세 내용</div>
+            <div class="popup_url_normal mu_form mgt10">
+                <textarea value="" readonly></textarea>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="pagingWrap">
-<? include_once PATH_INC_END.'page.php';?>
+    <? include_once PATH_INC_END.'page.php';?>
 </div>
 <script>
-$(document).ready(function(){
-	fnSearch = function() {
-		$("#searchForm").attr("action","").submit();
-	}
-})
+    $(document).ready(function(){
+
+        $('.pop_btn_01').on('click', function(){
+            const content = $(this).text().trim();
+            $('.popup_url_box textarea').val(content);
+            $('.popup_url_box').addClass('show');
+        })
+        // url 팝업 닫기
+        $('.popup_url_head .close').on('click', function(){
+            $('.popup_url_box').removeClass('show');
+        })
+
+        fnSearch = function() {
+            $("#searchForm").attr("action","").submit();
+        }
+    })
 </script>
 
 
--- aekyung_250520/loaksecure21/user/superpassHistory.php
+++ aekyung_250520/loaksecure21/user/superpassHistory.php
@@ -4,11 +4,11 @@ if(!defined('_HEROBOARD_'))exit;
 $search = "";
 
 if($_GET["superpass_check"]) {
-	$search .= " AND superpass_check = '".$_GET["superpass_check"]."' ";
+    $search .= " AND superpass_check = '".$_GET["superpass_check"]."' ";
 }
 
 if($_GET["kewyword"]) {
-	$search .= " AND ".$_GET["select"]." like '%".$_GET["kewyword"]."%' ";
+    $search .= " AND ".$_GET["select"]." like '%".$_GET["kewyword"]."%' ";
 }
 
 $total_sql  = " SELECT count(*) as cnt  ";
@@ -25,10 +25,10 @@ $list_page=20;
 $page_per_list=10;
 
 if(!strcmp($_GET['page'], '') || !strcmp($_GET['page'], '1')){
-	$page = '1';
+    $page = '1';
 }else{
-	$page = $_GET['page'];
-	$i = $i-($page-1)*$list_page;
+    $page = $_GET['page'];
+    $i = $i-($page-1)*$list_page;
 }
 
 $start = ($page-1)*$list_page;
@@ -43,11 +43,64 @@ $sql .= " ORDER BY s.hero_idx DESC LIMIT ".$start.",".$list_page;
 
 
 $list_res = sql($sql);
-?>	
+?>
 <form name="searchForm" id="searchForm" action="<?=PATH_HOME.'?'.get('page');?>">
-<input type="hidden" name="idx" value="<?=$_GET["idx"]?>" />
-<input type="hidden" name="board" value="<?=$_GET["board"]?>" />
-<table class="tbSearch">
+    <input type="hidden" name="idx" value="<?=$_GET["idx"]?>" />
+    <input type="hidden" name="board" value="<?=$_GET["board"]?>" />
+
+
+    <!-- 250625 슈퍼패스 지급내역 검색 필터 -->
+    <table class="searchBox">
+        <colgroup>
+            <col width="171px" />
+            <col width="*" />
+        </colgroup>
+        <tr>
+            <th>지급/미지급</th>
+            <td>
+                <div class="search_inner sup">
+                    <label class="akContainer">전체
+                        <input type="checkbox" name="superpass_chk" value="all">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">지급
+                        <input type="checkbox" name="superpass_chk" value="payment">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="akContainer">미지급
+                        <input type="checkbox" name="superpass_chk" value="unpaid">
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                검색
+            </th>
+            <td>
+                <div class="search_inner">
+                    <div class="select-wrap">
+                        <select name="select">
+                            <option value="none" <?=!isset($_GET["select"]) || $_GET["select"] == "none" ? "selected" : ""?>>선택</option>
+                            <option value="hero_nick" <?=$_GET["select"] == "m.hero_nick" ? "selected" : ""?>>닉네임</option>
+                            <option value="hero_id" <?=$_GET["select"] == "m.hero_id" ? "selected" : ""?>>아이디</option>
+                            <option value="hero_name" <?=$_GET["select"] == "m.hero_name" ? "selected" : ""?>>이름</option>
+                        </select>
+                    </div>
+                    <input class="search_txt" type="text" name="kewyword" value="<?=$_GET["kewyword"]?>"/>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <div class="btnGroupSearch_box">
+        <div class="btnGroupSearch">
+            <a href="javascript:;" onClick="fnSearch()" class="btnSearch">검색</a>
+        </div>
+    </div>
+
+    <!-- 250625 검색필터 기존 주석처리 -->
+    <!-- <table class="tbSearch">
 	<colgroup>
 		<col width="150px" />
 		<col width="*" />
@@ -68,100 +121,203 @@ $list_res = sql($sql);
 		    	<option value="hero_name" <?if(!strcmp($_REQUEST['select'], 'hero_name')){echo ' selected';}else{echo '';}?>>이름</option>
 		    	<option value="hero_id" <?if(!strcmp($_REQUEST['select'], 'hero_id')){echo ' selected';}else{echo '';}?>>아이디</option>
 	    	</select>
-	    	<input name="kewyword" type="text" value="<?echo $_REQUEST['kewyword'];;?>" class="kewyword">		
+	    	<input name="kewyword" type="text" value="<?echo $_REQUEST['kewyword'];;?>" class="kewyword">
 		</td>
 	</tr>
 </table>
 <div class="btnGroupSearch">
 	<a href="javascript:;" onClick="fnSearch()" class="btnSearch">검색</a>
-</div>
+</div> -->
 </form>
 
-<div class="view_title_box">
-	<p><label>슈퍼패스 조건</label></p>
-	<p>
-	1. 매달 처음 로그인할 때 지급 가능</br>
-	2. 로그인 시점에 3개월 이전에 패널티가 없어야 함</br>
-	3. 로그인 시점에 한달 전에 로그인한 기록이 있어야함 </br>
-	4. 블로그+영상 url 존재</br>
-	5. 한달이전에 등록한 글 또는 댓글 존재</br></br>
-	ex) 1번 조건이 성립되지 않으면 히스토리가 없음
-	</p>
+<div class="descWrap mgt30">
+    <p class="dw_tit"><label>슈퍼패스 조건</label></p>
+    <p class="dw_desc">
+        1. 매달 처음 로그인할 때 지급 가능</br>
+        2. 로그인 시점에 3개월 이전에 패널티가 없어야 함</br>
+        3. 로그인 시점에 한달 전에 로그인한 기록이 있어야함 </br>
+        4. 블로그+영상 url 존재</br>
+        5. 한달이전에 등록한 글 또는 댓글 존재</br></br>
+        ex) 1번 조건이 성립되지 않으면 히스토리가 없음
+    </p>
 </div>
 
-<div class="listExplainWrap mgb10">
-<label>총 </label> : <strong><?=number_format($total_data)?></strong>건
-</div>
+<div class="tableSection mgt30">
+    <div class="table_top">
+        <h2 class="table_tit">검색 결과</h2>
+        <p class="postNum"><span class="line"><?=number_format($search_total)?>개</span><span class="op_5">전체 <?=number_format($total_data)?>개</span></p>
+    </div>
+    <p class="table_desc"></p>
+    <div class="searchResultBox_container">
+        <table class="searchResultBox">
+            <colgroup>
+                <col width="45px" />
+                <col width="70px" />
+                <col width="70px" />
+                <col width="70px" />
+                <col width="60px" />
+                <col width="60px" />
+                <col width="60px" />
+                <col width="75px" />
+                <col width="75px" />
+                <col width="75px" />
+            </colgroup>
+            <thead>
+            <th>
+                <div class="">
+                    NO
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    이름
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    아이디
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    닉네임
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    패널티
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    한달 전 로그인
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    블로그/영상
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    작성글
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    지급우뮤
+                </div>
+            </th>
+            <th>
+                <div class="">
+                    로그인날짜
+                </div>
+            </th>
+            </thead>
+            <tbody>
+            <?
+            if($total_data > 0) {
+                while($list = mysql_fetch_assoc($list_res)) {
+                    $superpass_txt = "";
+                    if($list["superpass_check"] == "Y") {
+                        $superpass_txt = "지급";
+                    } else {
+                        $superpass_txt = "미지급";
+                    }
 
-<table class="t_list">
-<colgroup>
-	<col width="4%" />
-	<col width="8%" />
-	<col width="8%" />
-	<col width="8%" />
-	<col width="10%" />
-	<col width="10%" />
-	<col width="10%" />
-	<col width="10%" />
-	<col width="10%" />
-	<col width="*" />
-</colgroup>
-<thead>
-<tr>
-	<th>NO</th>
-	<th>이름</th>
-	<th>아이디</th>
-	<th>닉네임</th>
-	<th>패널티</th>
-	<th>한달 전 로그인</th>
-	<th>블로그/영상</th>
-	<th>작성글</th>
-	<th>지급유무</th>
-	<th>로그인날짜</th>
-</tr>
-</thead>
-<?
-if($total_data > 0) {
-while($list = mysql_fetch_assoc($list_res)) {
-	$superpass_txt = "";
-	if($list["superpass_check"] == "Y") {
-		$superpass_txt = "지급";
-	} else {
-		$superpass_txt = "미지급";
-	}
-?>
-<tr>
-	<td><?=$i?></td>
-	<td><?=$list["hero_name"]?></td>
-	<td><?=$list["hero_id"]?></td>
-	<td><?=$list["hero_nick"]?></td>
-	<td><?=$list["panelty_check"]?></td>
-	<td><?=$list["login_a_month_ago_check"]?></td>
-	<td><?=$list["blog_check"]?></td>
-	<td><?=$list["write_check"]?></td>
-	<td><?=$superpass_txt?></td>
-	<td><?=$list["hero_today"]?></td>
-</tr>
-<? 
-$i--;
-} 
-} else {
-?>
-<tr>
-	<td colspan="10">등록된 데이터가 없습니다.</td>
-</tr>
-<?}?>
-</table>
+                    // 패널티
+                    if($list['panelty_check'] == 'Y') $panelty_check = '○';
+                    else $panelty_check = '×';
+
+                    // 한달 전 로그인
+                    if($list['login_a_month_ago_check'] == 'Y') $monthAgo_check = '○';
+                    else $monthAgo_check = '×';
+
+                    // 블로그/영상
+                    if($list['blog_check'] == 'Y') $blog_check = '○';
+                    else $blog_check = '×';
+
+                    // 작성글
+                    if($list['write_check'] == 'Y') $write_check = '○';
+                    else $write_check = '×';
+
+                    ?>
+                    <tr>
+                        <td>
+                            <div class="table_result_no">
+                                <?=$i?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$list["hero_name"]?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$list["hero_id"]?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$list["hero_nick"]?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$panelty_check?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$monthAgo_check?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$blog_check?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$write_check?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_nick">
+                                <?=$superpass_txt?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="table_result_create">
+                                <?=$list["hero_today"]?>
+                            </div>
+                        </td>
+                    </tr>
+                    <?
+                    $i--;
+                }
+            } else {
+                ?>
+                <tr>
+                    <td colspan="10" class="no_data">등록된 데이터가 없습니다.</td>
+                </tr>
+            <?}?>
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <div class="pagingWrap">
-<? include_once PATH_INC_END.'page.php';?>
+    <? include_once PATH_INC_END.'page.php';?>
 </div>
 <script>
-$(document).ready(function(){
-	fnSearch = function() {
-		$("#searchForm").attr("action","").submit();
-	}
-})
+    $(document).ready(function(){
+
+        fnSearch = function() {
+            $("#searchForm").attr("action","").submit();
+        }
+    })
 </script>
 
 
